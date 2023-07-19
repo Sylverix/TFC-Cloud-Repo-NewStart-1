4cbc79accce3d9fc807176f2811c55d264fafec1
Sylverix <124088369+Sylverix@users.noreply.github.com>
2023-07-19 11:32:39 -0400

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "latest-linux-ec2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "terraform-web-server" {
+  ami = data.aws_ami.latest-linux-ec2.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform Web Server"
+  }
+}
